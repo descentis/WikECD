44824698c2b5b47d85c7d758eea0fb755d0932ba
descentis <mt4descentis@gmail.com>
2018-05-30 15:03:15 +0530

    Adding all files first time

---
+++ js/front.js
@@ -0,0 +1,170 @@
+if ($.cookie("theme_csspath")) {
+    $('link#theme-stylesheet').attr("href", $.cookie("theme_csspath"));
+}
+
+$(function () {
+
+    sticky();
+    map();
+    utils();
+    demo();
+
+});
+
+/* for demo purpose only - can be deleted */
+
+function demo() {
+
+    if ($.cookie("theme_csspath")) {
+        $('link#theme-stylesheet').attr("href", $.cookie("theme_csspath"));
+    }
+
+    $("#colour").change(function () {
+
+        if ($(this).val !== '') {
+
+            var colour = $(this).val();
+            
+            var theme_csspath = 'css/style.' + $(this).val() + '.css';
+            $('link#theme-stylesheet').attr("href", theme_csspath);
+            $.cookie("theme_csspath", theme_csspath, {expires: 365, path: '/'});
+        }
+
+        return false;
+    });
+}
+
+function sticky() {
+
+    $(".header").sticky();
+
+}
+
+/* =========================================
+ *  map 
+ *  =======================================*/
+
+function map() {
+
+    var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
+    map = new GMaps({
+        el: '#map',
+        lat: -12.043333,
+        lng: -77.028333,
+        zoomControl: true,
+        zoomControlOpt: {
+            style: 'SMALL',
+            position: 'TOP_LEFT'
+        },
+        panControl: false,
+        streetViewControl: false,
+        mapTypeControl: false,
+        overviewMapControl: false,
+        scrollwheel: false,
+        draggable: false,
+        styles: styles
+    });
+
+    var image = 'img/marker.png';
+
+    map.addMarker({
+        lat: -12.043333,
+        lng: -77.028333,
+        icon: image,
+        title: '',
+        infoWindow: {
+            content: '<p>HTML Content</p>'
+        }
+    });
+}
+
+/* =========================================
+ *  UTILS
+ *  =======================================*/
+
+function utils() {
+
+    /* tooltips */
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+    /* click on the box activates the radio */
+
+    $('#checkout').on('click', '.box.shipping-method, .box.payment-method', function (e) {
+        var radio = $(this).find(':radio');
+        radio.prop('checked', true);
+    });
+    /* click on the box activates the link in it */
+
+    $('.box.clickable').on('click', function (e) {
+
+        window.location = $(this).find('a').attr('href');
+    });
+    /* external links in new window*/
+
+    $('.external').on('click', function (e) {
+
+        e.preventDefault();
+        window.open($(this).attr("href"));
+    });
+    /* animated scrolling */
+
+    $('.scroll-to, #navigation a').click(function (event) {
+        event.preventDefault();
+        var full_url = this.href;
+        var parts = full_url.split("#");
+        var trgt = parts[1];
+
+        $('body').scrollTo($('#' + trgt), 800, {offset: -50});
+
+    });
+
+}
+
+$.fn.alignElementsSameHeight = function () {
+    $('.same-height-row').each(function () {
+
+        var maxHeight = 0;
+        var children = $(this).find('.same-height');
+        children.height('auto');
+        if ($(window).width() > 768) {
+            children.each(function () {
+                if ($(this).innerHeight() > maxHeight) {
+                    maxHeight = $(this).innerHeight();
+                }
+            });
+            children.innerHeight(maxHeight);
+        }
+
+        maxHeight = 0;
+        children = $(this).find('.same-height-always');
+        children.height('auto');
+        children.each(function () {
+            if ($(this).innerHeight() > maxHeight) {
+                maxHeight = $(this).innerHeight();
+            }
+        });
+        children.innerHeight(maxHeight);
+    });
+}
+
+$(window).load(function () {
+
+    windowWidth = $(window).width();
+
+    $(this).alignElementsSameHeight();
+
+
+});
+$(window).resize(function () {
+
+    newWindowWidth = $(window).width();
+
+    if (windowWidth !== newWindowWidth) {
+        setTimeout(function () {
+            $(this).alignElementsSameHeight();
+        }, 100);
+        windowWidth = newWindowWidth;
+    }
+
+});
---
+++ readme.txt
@@ -0,0 +1,51 @@
+################################################################################################
+
+Documentation for Agency Theme by Bootstrapious - http://bootstrapious.com, rel. 2015/04/10
+
+################################################################################################
+
+Hi,
+
+thank you for downloading. Have fun and tell your friends about us ;)
+
+Ondrej from Bootstrapious
+
+
+CSS
+----------
+
+The theme stylesheet is css/style.default.css. If you want to make any changes, you can do it here or better to override it in custom.css so you can update the original theme stylesheet if an updated is released. If you want to change colour theme, change the CSS path in the <head> to e.g. css/style.red.css.
+
+Javascript
+----------
+
+Apart from Bootstrap JS components majority of JS is located in /js/front.js. If you want to make any js settings finetuning, you can do it here.
+
+Credits
+---------
+
+- Botstrap 3.3.2
+- Font Awesome 4.3.0
+- Google Fonts - Roboto Condensed and Open Sans
+- Heisengberg Media - intro photo
+
+
+---------------------
+ LICENSE CONDITIONS
+---------------------
+
+You are completely free to use this template for your personal use or as a work for your client as long as you keep the link at the template footer pointing to us and our partner. 
+
+If you would prefer removing the backlink from the theme footer, please donate (https://bootstrapious.com/donate) to support themes' development. Suggested amount per template is $10.
+
+However you cannot redistribute the template nor its derivatives on the internet - neither for free or commercially (e.g. selling it on template marketplace).
+
+Thank you for understanding and respecting the license conditions.
+
+If more info needed, contact me at hello@bootstrapious.com.
+
+---------------------
+ GET IN TOUCH ;)
+---------------------
+
+https://twitter.com/bootstrapious | https://google.com/+Bootstrapious1 | https://www.facebook.com/bootstrapious | hello@bootstrapious.com
