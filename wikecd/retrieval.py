9c9cb0b78018c3b5bf24ff915486d194808148aa
descentis <mt4descentis@gmail.com>
2020-09-07 14:40:44 +0530

    created the handeler class in retrieval method

--- wikecd/retrieval.py
+++ wikecd/retrieval.py
@@ -6,3 +6,21 @@ Created on Sun Sep  6 20:56:47 2020
 @author: descentis
 """
 
+import os
+from multiprocessing import Process, Lock
+import time
+import numpy as np
+import glob
+import difflib
+import xml.etree.ElementTree as ET
+import math
+import textwrap
+import html
+import requests
+import io
+
+
+class wikiRetrieval(object):
+    '''
+    The class with organize the full revision history of Wikipedia dataset
+    '''
