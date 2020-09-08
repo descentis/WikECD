ae2466441110fceaa43b475921479b78099219d2
descentis <mt4descentis@gmail.com>
2020-09-08 12:03:43 +0530

    added the indent method in the retrieval file

--- wikecd/retrieval.py
+++ wikecd/retrieval.py
@@ -24,3 +24,21 @@ class wikiRetrieval(object):
     '''
     The class with organize the full revision history of Wikipedia dataset
     '''
+    
+    instance_id = 1
+    
+    
+    def indent(self,elem, level=0):
+        i = "\n" + level*"  "
+        if len(elem):
+            if not elem.text or not elem.text.strip():
+                elem.text = i + "  "
+            if not elem.tail or not elem.tail.strip():
+                elem.tail = i
+            for elem in elem:
+                self.indent(elem, level+1)
+            if not elem.tail or not elem.tail.strip():
+                elem.tail = i
+        else:
+            if level and (not elem.tail or not elem.tail.strip()):
+                elem.tail = i    
