b637e523f02d52d0631de838d1b6bf6932546aaf
descentis <mt4descentis@gmail.com>
2020-10-12 23:38:33 +0530

    minor edits

--- tests/results/bar_plot.py
+++ tests/results/bar_plot.py
@@ -18,11 +18,12 @@ plt.bar(y_pos, height, color=(0.1, 0.1, 0.1, 0.1),  edgecolor='blue')
 
 label = ['93.25%', '93.60%', '95.02%', '95.38%']
 plt.text(x=0-0.2, y=46 - 6, s = '46%', size=12)
-plt.text(x=1-0.3, y=93.25 - 6, s = '93.25%', size=12)
-plt.text(x=2-0.3, y=93.60 - 6, s = '93.60%', size=12)
-plt.text(x=3-0.3, y=95.02 - 6, s = '95.02%', size=12)
-plt.text(x=4-0.3, y=95.38 - 6, s = '95.38%', size=12)
+plt.text(x=1-0.35, y=93.25 - 6, s = '93.25%', size=12)
+plt.text(x=2-0.35, y=93.60 - 6, s = '93.60%', size=12)
+plt.text(x=3-0.35, y=95.02 - 6, s = '95.02%', size=12)
+plt.text(x=4-0.35, y=95.38 - 6, s = '95.38%', size=12)
 
 
-plt.xticks(y_pos, bars, rotation=30)
+plt.xticks(y_pos, bars, fontsize=8)
+plt.savefig('overall_compression.png', dpi=800)
 plt.show()
